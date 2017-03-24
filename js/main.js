79259ba96d60a56b11b76f41e83d3294be7d1102
Alexiz Knöös <qwastanja@hotmail.com>
2017-03-24 19:57:35 +0100

    Template för vinnar popupen. Har skrivit html, css och js för den.

--- js/main.js
+++ js/main.js
@@ -27,6 +27,8 @@ function start(){
 	$('#submitFormCol').append(submitForm());
 
 	$('#diceTableCol').append(diceTable());
+	$('body').append(winnerTemplate());
+
 	rollDice();
 
 	submitPlayer();
@@ -159,6 +161,26 @@ function submitPlayer(){
 }
 //hur ska jag få rätt totalsumma som tillhör en viss spelare?
 
+//Javascript till vinnar popup börjar här
+var moveForce = 30; // max popup movement in pixels
+var rotateForce = 20; // max popup rotation in deg
+
+$(document).mousemove(function(e) {
+    var docX = $(document).width();
+    var docY = $(document).height();
+    
+    var moveX = (e.pageX - docX/2) / (docX/2) * -moveForce;
+    var moveY = (e.pageY - docY/2) / (docY/2) * -moveForce;
+    
+    var rotateY = (e.pageX / docX * rotateForce*2) - rotateForce;
+    var rotateX = -((e.pageY / docY * rotateForce*2) - rotateForce);
+    
+    $('.popup')
+        .css('left', moveX+'px')
+        .css('top', moveY+'px')
+        .css('transform', 'rotateX('+rotateX+'deg) rotateY('+rotateY+'deg)');
+});
+//Javascript till vinnar popup slutar här
 
 // Events
 
