8663d42e6700753411d81b4043fd2d31e6de790c
ConradLetelier <Conrad@Letelier.email>
2017-03-28 14:55:16 +0200

    Poäng kalkulator färdig

--- js/main.js
+++ js/main.js
@@ -138,36 +138,35 @@ function totalCalc(){
 
 dice=[{
     name:1,
-    value:3,
-    saved:true
+    value:6,
+    saved:false
 },
 {
     name:2,
-    value:3,
+    value:6,
     saved:false
 },
 {
     name:3,
-    value:1,
+    value:6,
     saved:true
 },
 {
     name:4,
-    value:4,
+    value:6,
     saved:true
 },
 {
     name:5,
-    value:1,
+    value:6,
     saved:true
 }];
 
 //Adderar poäng till formuläret
-function addToScore(a){
+function addToScore(thisDiv){
     var pointAdded = 0;
-    var x = a.previousElementSibling.innerHTML;
-    var p = parseInt(a.previousElementSibling.id);
-    console.log(p);
+    var x = thisDiv.previousElementSibling.innerHTML;
+    var p = parseInt(thisDiv.previousElementSibling.id);
     if(x==x){
         for(var i = 0; i < dice.length; i++){
             if(dice[i].value==p){
@@ -175,45 +174,188 @@ function addToScore(a){
             }
         }
     }
-    $(a).text(pointAdded);
+    $(thisDiv).text(pointAdded);
     totalCalc();
     newRound();
 }
-function addToScoreAdvanced(a){
+function addToScoreAdvanced(thisDiv){
+    //Variables that are used in the calculator
     var result = [];
     var result1= [];
     var pointAdded = 0;
     var count = 0;
-    var x = a.previousElementSibling.innerHTML;
-        
+    var x = thisDiv.previousElementSibling.innerHTML;
+    //
+        //ONE PAIR
+    //
     if(x=="Ett Par"){
+        //See if there are any multiply values in the Dice array
         $.each(dice, function (key, point) {
             if($.inArray(point.value, result) === -1) {
                 result.push(point.value);
             }else{
                 result1.push(point.value,point.saved)
             }
-        }); 
-        if(result1[1]==null){
+        });
+        //To make sure you cant trick the system, we check several options which needs to be true
+        if(result1[1]==null&&result1[0]<7){
             pointAdded=result1[0].value*2
-            console.log(pointAdded)
         }else{
             if(result1[1]==true&&result1[3]==true){
                 pointAdded=Math.min(result1[0],result1[2])*2
-                console.log("1")
             }else if(result1[1]==true){
                 pointAdded=result1[0]*2
-                console.log("2")
-            }else{
+            }else if(result1[3]==true){
                 pointAdded=result1[2]*2
-                console.log(result1)
+            }else if(result1.length==0){
+            }
+            else{
+                pointAdded=result1[4]*2
             }
         }
-            $(a).text(pointAdded);
+            $(thisDiv).text(pointAdded);
             totalCalc();
             newRound();
         
-    }else if(x=="Två Par"){
+    }
+    //
+        //TWO PAIR
+    //
+    else if(x=="Två Par"){
+        //See if there are any multiply values in the Dice array
+        $.each(dice, function (key, point) {
+            if($.inArray(point.value, result) === -1) {
+                result.push(point.value);
+            }else{
+                result1.push(point.value);
+            }
+        });
+        //To make sure you cant trick the system, we check several options which needs to be true
+        if(result1[1]==null){   
+        }else if(result1[2]==null){
+         pointAdded = (result1[0]*2) + (result1[1]*2) 
+        }
+        else{
+            pointAdded = (result1[2]*2) + (result1[1]*2)
+        }
+            $(thisDiv).text(pointAdded);
+            totalCalc();
+            newRound();
+        }
+    //
+        //THREE OF A KIND
+    //
+    else if(x=="Triss"){
+            //Check if any values match eachother in the Dice array
+            for (var i = 0; i < dice.length;i++){
+                var count = 0;
+                for(var j = 0; j < dice.length; j++){
+                    //Keep a count how many times they match
+                    if(dice[i].value == dice[j].value){
+                        count++;
+                        //If they match more than two times, this point is valid
+                        if(count>2){
+                            pointAdded=dice[i].value*3
+                        }
+                    }
+                }
+            }
+            $(thisDiv).text(pointAdded);
+            totalCalc();
+            newRound();
+        }
+    //
+        //FOUR OF A KIND
+    //
+    else if(x=="Fyrtal"){
+            //Check if any values match eachother in the Dice array
+            for (var i = 0; i < dice.length;i++){
+                var count = 0;
+                for(var j = 0; j < dice.length; j++){
+                    //Keep a count how many times they match
+                    if(dice[i].value == dice[j].value){
+                        count++;
+                        //If they match more than three times, this point is valid
+                        if(count>3){
+                            pointAdded=dice[i].value*4
+                        }
+                    }
+                }
+            }
+            $(thisDiv).text(pointAdded);
+            totalCalc();
+            newRound();            
+        }
+    //
+        //SMALL LADDER
+    //
+        else if(x=="Liten Stege"){
+            //We need all of theese counts to be 1. You could think of them as booleans, i just choosed numbers instead
+            var count1 = 0;
+            var count2 = 0;
+            var count3 = 0;
+            var count4 = 0;
+            var count5 = 0;
+        for(var i = 0; i < dice.length; i++){
+            //If the value is valid, count is changed
+            if(dice[i].value==1){
+                count1=1;
+            }if(dice[i].value==2){
+                count2=1;
+            }if(dice[i].value==3){
+                count3=1;
+            }if(dice[i].value==4){
+                count4=1;
+            }if(dice[i].value==5){
+                count5=1;
+            }
+        }   
+            //We add the counts
+            count += (count1+count2+count3+count4+count5)
+            //If all count has been triggered, point is valid
+            if(count==5){
+            pointAdded=15;
+        }
+            $(thisDiv).text(pointAdded);
+            totalCalc();
+            newRound();
+        }
+    //
+        //BIG LADDER
+    //
+        else if(x=="Stor Stege"){
+            //Same as the "Small Ladder" just that one dice.value need to be 6 instead of 1
+            var count1 = 0;
+            var count2 = 0;
+            var count3 = 0;
+            var count4 = 0;
+            var count5 = 0;
+        for(var i = 0; i < dice.length; i++){
+            if(dice[i].value==6){
+                count1=1;
+            }if(dice[i].value==2){
+                count2=1;
+            }if(dice[i].value==3){
+                count3=1;
+            }if(dice[i].value==4){
+                count4=1;
+            }if(dice[i].value==5){
+                count5=1;
+            }
+        }
+            count += (count1+count2+count3+count4+count5)
+            if(count==5){
+            pointAdded=20;
+        }
+            $(thisDiv).text(pointAdded);
+            totalCalc();
+            newRound();
+        }
+    //
+        //FULL HOUSE
+    //
+    else if(x=="Kåk"){
+        //Check multiply values
         $.each(dice, function (key, point) {
             if($.inArray(point.value, result) === -1) {
                 result.push(point.value);
@@ -221,16 +363,60 @@ function addToScoreAdvanced(a){
                 result1.push(point.value);
             }
         });     
-        if(result1[1]==null){
-        }else{
-            pointAdded = (result1[0]*2) + (result1[1]*2)
+            //You can log result1 for better understanding
+                // This array needs to have 3 numbers
+            if(result1.length==3){
+                //Formulas to see which multiply values is going to multiply 3 and 2
+                if(result1[0]==result1[1]&&result1[0]==result1[2]){
+                }else{
+                    if(result1[0]==result1[1]){
+                        pointAdded= (result1[0]*3)+(result1[2]*2)
+                    }if(result1[0]==result1[2]){
+                        pointAdded= (result1[0]*3)+(result1[1]*2)
+                    }
+                    else{
+                        pointAdded= (result1[1]*3)+(result1[0]*2)
+                    }
+                }
+            }
+            $(thisDiv).text(pointAdded);
+            totalCalc();
+            newRound();            
         }
-            $(a).text(pointAdded);
+    //
+        //CHANCE
+    //
+    else if(x=="Chans"){
+            //Add all values to point!
+            for(var i = 0; i < dice.length;i++){
+                pointAdded+=dice[i].value
+            }
+            $(thisDiv).text(pointAdded);
             totalCalc();
             newRound();
-        }            
+        }
+    //
+        //YATZY
+    //
+    else if(x=="Yatzy"){
+        //Same is the "LADDERS" but modified
+            for (var i = 0; i < dice.length;i++){
+                var count = 0;
+                for(var j = 0; j < dice.length; j++){
+                    if(dice[i].value == dice[j].value){
+                        count++;
+                        //Modified
+                        if(count>4){
+                            pointAdded=dice[i].value*6
+                        }
+                    }
+                }
+            $(thisDiv).text(pointAdded);
+            totalCalc();
+            newRound();
+            }            
+        }
 }
-    
 
 
 function newRound(){
